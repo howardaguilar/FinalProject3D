3c85e8df58f2b8fb9c9337d966efd00c8b53f0c2
howardaguilar <haguilar@csumb.edu>
2020-11-17 19:44:17 -0800

    Draggable objects and test questions

---
+++ Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+    GameObject target;
+    bool isMouseDragging;
+    Vector3 screenPosition;
+    Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hitInfo;
+            target = ReturnClickedObject(out hitInfo);
+            if (target != null)
+            {
+                isMouseDragging = true;
+                Debug.Log("our target position :" + target.transform.position);
+                //Here we Convert world position to screen position.
+                screenPosition = Camera.main.WorldToScreenPoint(target.transform.position);
+                offset = target.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPosition.z));
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            isMouseDragging = false;
+        }
+
+        if (isMouseDragging)
+        {
+            //tracking mouse position.
+            Vector3 currentScreenSpace = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPosition.z);
+
+            //convert screen position to world position with offset changes.
+            Vector3 currentPosition = Camera.main.ScreenToWorldPoint(currentScreenSpace) + offset;
+
+            //It will update target gameobject's current postion.
+            target.transform.position = currentPosition;
+        }
+
+    }
+
+    GameObject ReturnClickedObject(out RaycastHit hit)
+    {
+        GameObject targetObject = null;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray.origin, ray.direction * 10, out hit))
+        {
+            targetObject = hit.collider.gameObject;
+        }
+        return targetObject;
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public TextMeshProUGUI question;
+    bool answerOne = false;
+    bool answerTwo = false;
+    bool answerThree = false;
+    // Clicking Object
+    GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        questionOne();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hitInfo;
+            target = ReturnClickedObject(out hitInfo);
+            if (target != null && target.name == "Sphere" && answerOne == false)
+            {
+                answerOne = true;
+                questionTwo();
+            }
+        }
+    }
+
+    void questionOne()
+    {
+        question.text = "What is the true shape of our world?";
+    }
+
+    void questionTwo()
+    {
+        question.text = "Is gravity really there?";
+    }
+
+    GameObject ReturnClickedObject(out RaycastHit hit)
+    {
+        GameObject targetObject = null;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray.origin, ray.direction * 10, out hit))
+        {
+            targetObject = hit.collider.gameObject;
+        }
+        return targetObject;
+    }
+}
