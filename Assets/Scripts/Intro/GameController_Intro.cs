d290b9a616c90d7b424188380e14ca9c19da35b7
Howard Aguilar <haguilar@csumb.edu>
2020-11-19 17:43:43 -0800

    Added button to intro, swap scene

---
+++ Assets/Scripts/Intro/GameController_Intro.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameController_Intro : MonoBehaviour
+{
+    public Button start;
+    public GameObject gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = start.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void TaskOnClick()
+    {
+        DontDestroyOnLoad(gameManager);
+        SceneManager.LoadScene("Level_1");
+    }
+}
