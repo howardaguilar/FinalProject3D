df451710b95c8fc9f8f5f8c8bb0f4b4d236f6b1b
MikalWhaley <mwhaley@csumb.edu>
2020-12-07 23:59:58 -0800

    did transition 2 and level 3 and ending

---
+++ Assets/Scripts/Level_3/FallingObjects.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingObjects : MonoBehaviour
+{
+    public float fallSpeed = 8.0f;
+    public float spinSpeed = 250.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * fallSpeed * Time.deltaTime, Space.World);
+        transform.Rotate(Vector3.forward, spinSpeed * Time.deltaTime);
+    }
+
+  
+    
+
+}
